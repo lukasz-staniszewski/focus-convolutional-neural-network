Better classifier and fix images normalization

src/data_utils/data_loaders/ClassifierDataLoader.py
@@ -1,10 +1,8 @@
-from email.mime import image
 from pathlib import Path
-from torchvision import transforms
 from base import BaseDataLoader
 from data_utils.data_sets import ClassifierDataset
 from torch.utils.data import ConcatDataset
-from typing import List, Any
+from typing import Tuple
 import torchvision.transforms as T
 import pandas as pd
 from copy import deepcopy
@@ -16,6 +14,8 @@ class ClassifierDataLoader(BaseDataLoader):
         images_dir: str,
         batch_size: int,
         csv_path: str,
+        transform_mean: Tuple[float] = None,
+        transform_std: Tuple[float] = None,
         shuffle: bool = True,
         validation_split: float = 0.0,
         num_workers: int = 1,
@@ -23,12 +23,12 @@ class ClassifierDataLoader(BaseDataLoader):
         balance: bool = False,
     ):
         self.images_dir = images_dir
-        self.transform = transforms.Compose(transforms=[])
-        self.transform_aug = transforms.Compose(
-            transforms=[T.RandomHorizontalFlip(0.5), T.RandomErasing(0.5),]
-        )
         self.csv_path = csv_path
         self.balance = balance
+
+        self.combine_transforms(
+            transform_mean=transform_mean, transform_std=transform_std
+        )
 
         if is_test:
             self.dataset = ClassifierDataset(
@@ -40,7 +40,9 @@ class ClassifierDataLoader(BaseDataLoader):
             shuffle = False
         else:
             self.csv_aug_path = (
-                self.balance_lower_class() if self.balance else self.csv_path
+                self.balance_lower_class()
+                if self.balance
+                else self.csv_path
             )
             self.dataset = ConcatDataset(
                 [
@@ -58,7 +60,11 @@ class ClassifierDataLoader(BaseDataLoader):
             )
 
         super().__init__(
-            self.dataset, batch_size, shuffle, validation_split, num_workers,
+            self.dataset,
+            batch_size,
+            shuffle,
+            validation_split,
+            num_workers,
         )
 
     def balance_lower_class(self) -> Path:
@@ -69,7 +75,44 @@ class ClassifierDataLoader(BaseDataLoader):
         freq_cls = int(n_0 <= n_1)
         cnt_diff = abs(n_0 - n_1)
 
-        df_aug = df_orig[df_orig["label"] != freq_cls].sample(cnt_diff, replace=True)
+        df_aug = df_orig[df_orig["label"] != freq_cls].sample(
+            cnt_diff, replace=True
+        )
         new_path = deepcopy(self.csv_path).replace(".csv", "_aug.csv")
         df_aug.to_csv(new_path, index=False, header=True)
         return new_path
+
+    def combine_transforms(
+        self,
+        transform_mean: Tuple[float] = None,
+        transform_std: Tuple[float] = None,
+    ) -> None:
+        if transform_mean and transform_std:
+            self.transform = T.Compose(
+                transforms=[
+                    T.ToPILImage(),
+                    T.ToTensor(),
+                    T.Normalize(transform_mean, transform_std),
+                ]
+            )
+            self.transform_aug = T.Compose(
+                transforms=[
+                    T.ToPILImage(),
+                    T.ToTensor(),
+                    T.Normalize(transform_mean, transform_std),
+                    T.RandomHorizontalFlip(0.5),
+                    T.RandomErasing(0.5),
+                ]
+            )
+        else:
+            self.transform = T.Compose(
+                transforms=[T.ToPILImage(), T.ToTensor()]
+            )
+            self.transform_aug = T.Compose(
+                transforms=[
+                    T.ToPILImage(),
+                    T.ToTensor(),
+                    T.RandomHorizontalFlip(0.5),
+                    T.RandomErasing(0.5),
+                ]
+            )

src/models/Classifier.py
@@ -16,7 +16,7 @@ class Classifier(BaseModel):
                 stride=(2, 2),
             ),
             nn.ReLU(),
-            nn.Dropout2d(0.7),
+            nn.Dropout2d(0.1),
         )
         self.layer2 = nn.Sequential(
             nn.Conv2d(
@@ -37,7 +37,7 @@ class Classifier(BaseModel):
                 stride=(2, 2),
             ),
             nn.ReLU(),
-            nn.Dropout2d(0.6),
+            nn.Dropout2d(0.1),
             nn.MaxPool2d(kernel_size=3, stride=3),
         )
         self.layer4 = nn.Sequential(
@@ -48,19 +48,20 @@ class Classifier(BaseModel):
                 stride=(1, 1),
             ),
             nn.ReLU(),
-            nn.Dropout2d(0.7),
+            nn.Dropout2d(0.15),
             nn.BatchNorm2d(120),
             nn.MaxPool2d(kernel_size=3, stride=3),
         )
         self.fc = nn.Sequential(
             nn.Linear(in_features=1080, out_features=650),
             nn.ReLU(),
-            nn.Dropout(0.7),
+            nn.Dropout(0.5),
             nn.Linear(in_features=650, out_features=300),
             nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(in_features=300, out_features=50),
             nn.ReLU(),
-            nn.Dropout(0.7),
+            nn.Dropout(0.5),
             nn.Linear(in_features=50, out_features=1),
             nn.Sigmoid(),
         )