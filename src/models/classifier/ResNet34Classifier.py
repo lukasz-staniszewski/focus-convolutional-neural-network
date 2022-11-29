d1037f11c2ab9a614292f12de5cc8ec0a0b1e2ee
Łukasz Staniszewski <lukaszstaniszewski10@gmail.com>
2022-11-29 18:34:23 +0100

    added 34 resnet version

---
+++ src/models/classifier/ResNet34Classifier.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+from base import BaseModel
+from torchvision import models
+from torchvision.models import ResNet34_Weights
+import torch
+
+
+class ResNet34Classifier(BaseModel):
+    def __init__(self, n_classes=1) -> None:
+        super().__init__()
+        self.n_classes = n_classes
+
+        self.model = models.resnet34(weights=ResNet34_Weights.DEFAULT)
+        res_fc_out = self.model.fc.out_features
+
+        self.fc_out = nn.Sequential(
+            nn.Linear(in_features=res_fc_out, out_features=500),
+            nn.ReLU(),
+            nn.Dropout(0.7),
+            nn.Linear(in_features=500, out_features=50),
+            nn.ReLU(),
+            nn.Dropout(0.6),
+            nn.Linear(in_features=50, out_features=self.n_classes),
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        print(f"{x.shape=}")
+        x = self.fc_out(x)
+        return x
+
+    def get_prediction(self, output):
+        _, prediction = torch.max(output, 1)
+        return prediction
